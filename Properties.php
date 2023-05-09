52eeca2efc956bbfbf84ab92f3172c13a87b5fbe
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-10 06:36:35 +0700

    add Properties.php

---
+++ Properties.php
@@ -0,0 +1,18 @@
+<?php
+  require_once "data/Person.php";
+
+  $person = new Person();
+  $person->name = "Eko";
+  $person->address = "Subang";
+  $person->country = "Indonesia";
+  var_dump($person);
+
+  echo "Name : $person->name" . PHP_EOL;
+  echo "Address : $person->address" . PHP_EOL;
+  echo "Country : $person->country" . PHP_EOL;
+
+  $person2 = new Person();
+  $person2->name = "Budi";
+  $person2->address = "Jakarta";
+  $person2->country = "Indonesia";
+  var_dump($person2);
