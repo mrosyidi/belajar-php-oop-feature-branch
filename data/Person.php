8e6c7031c433c7b9df1b85b5825e9bfa3a2a359b
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-10 12:30:45 +0700

    update Person.php membuat construct

--- data/Person.php
+++ data/Person.php
@@ -6,6 +6,12 @@
     var ?string $address = null;
     var string $country = "Indonesia";
 
+    public function __construct(string $name, ?string $address)
+    {
+      $this->name = $name;
+      $this->address = $address;
+    }
+
     function sayHello(?string $name)
     {
       if(is_null($name)){
