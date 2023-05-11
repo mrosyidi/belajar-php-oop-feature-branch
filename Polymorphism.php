ad82252bfa4a3b1b051030ed3100de8a29e228a2
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-11 09:34:47 +0700

    add Polymorphism.php

---
+++ Polymorphism.php
@@ -0,0 +1,12 @@
+<?php
+  require_once "data/Programmer.php";
+
+  $company = new Company();
+  $company->programmer = new Programmer("Eko");
+  var_dump($company);
+
+  $company->programmer = new BackendProgrammer("Eko");
+  var_dump($company);
+
+  $company->programmer = new FrontendProgrammer("Eko");
+  var_dump($company);
