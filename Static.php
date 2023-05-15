6e4efed31a56363104b44e7a01f8893f7d9eeefe
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-16 06:41:12 +0700

    update Static.php static function

--- Static.php
+++ Static.php
@@ -6,3 +6,6 @@
   echo MathHelper::$name . PHP_EOL;
   MathHelper::$name = "Eko Kurniawan";
   echo MathHelper::$name . PHP_EOL;
+
+  $result = MathHelper::sum(10, 10, 10, 10, 10);
+  echo "Result : $result" . PHP_EOL;
