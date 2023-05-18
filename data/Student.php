a2b70040fe608acb89d919c4bc26d95e41e6404a
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-18 08:03:42 +0700

    update Student.php tostring function

--- data/Student.php
+++ data/Student.php
@@ -15,4 +15,9 @@
     {
       unset($this->sample);
     }
+
+    public function __toString(): string
+    {
+      return "Student id:$this->id, name:$this->name, value:$this->value";
+    }
   }
