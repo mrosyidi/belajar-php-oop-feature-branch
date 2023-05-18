301dbcaeabbd0f937a807ac4bc675588d98aad0a
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-18 07:30:56 +0700

    ObjectCloning.php

---
+++ ObjectCloning.php
@@ -0,0 +1,11 @@
+<?php
+  require_once "data/Student.php";
+
+  $student1 = new Student();
+  $student1->id = "1";
+  $student1->name = "Eko";
+  $student1->value = 100;
+  var_dump($student1);
+
+  $student2 = clone $student1;
+  var_dump($student2);
