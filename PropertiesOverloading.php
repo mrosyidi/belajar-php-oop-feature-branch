ba0b3ddd1d7378331e6a5b276ecc9d794cc03566
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-19 13:03:23 +0700

    update PropertiesOverloading.php

--- PropertiesOverloading.php
+++ PropertiesOverloading.php
@@ -22,6 +22,18 @@
     {
       unset($this->properties[$name]);
     }
+
+    public function __call($name, $arguments)
+    {
+      $join = join(",", $arguments);
+      echo "Call functon $name with arguments $join" . PHP_EOL;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+      $join = join(",", $arguments);
+      echo "Call static function $name with arguments $join" . PHP_EOL;
+    }
   }
 
   $zero = new Zero();
@@ -32,3 +44,5 @@
   echo "First Name : $zero->firstName" . PHP_EOL;
   echo "Middle Name : $zero->middleName" . PHP_EOL;
   echo "Last Name : $zero->lastName" . PHP_EOL;
+  $zero->sayHello("Eko", "Khannedy");
+  Zero::sayHello("Eko", "Khannedy");
