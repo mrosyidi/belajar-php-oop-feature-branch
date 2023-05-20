bd38ee40a80e59131f9074355b49d62bca93f774
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-20 13:12:43 +0700

    update DateTime.php date interval

--- DateTime.php
+++ DateTime.php
@@ -2,4 +2,8 @@
   $dateTime = new DateTime();
   $dateTime->setDate(1998, 1, 28);
   $dateTime->setTime(10, 10, 10, 10);
+  $dateTime->add(new DateInterval("P1Y"));
+  $minusOneMonth = new DateInterval("P1M");
+  $minusOneMonth->invert = true;
+  $dateTime->add($minusOneMonth);
   var_dump($dateTime);
