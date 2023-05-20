e9926722f55e0ca584d16256800a8e27aa02236c
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-21 01:58:08 +0700

    update Exception.php multiple try catch

--- Exception.php
+++ Exception.php
@@ -4,10 +4,14 @@
   require_once "helper/Validation.php";
 
   $loginRequest = new LoginRequest();
+  $loginRequest->username = "  ";
+  $loginRequest->password = "  ";
 
   try{
     validateLoginRequest($loginRequest);
   }catch(ValidationException $exception){
     echo "Validation Error : {$exception->getMessage()}" . PHP_EOL;
+  }catch(Exception $exception){
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
   }
   echo "VALID" . PHP_EOL;
