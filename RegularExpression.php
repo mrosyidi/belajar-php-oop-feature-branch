6d0e224ca04d39c557e5999d3f06bbeca48ce9ec
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-21 02:44:49 +0700

    update RegularExpression.php split

--- RegularExpression.php
+++ RegularExpression.php
@@ -6,3 +6,6 @@
 
   $result = preg_replace("/anjing|bangsat/i", "***", "dasar lu anjing dan bangsat!");
   var_dump($result);
+
+  $result = preg_split("/[\s,-]/", "Eko Kurniawan Khannedy Programmer Zaman-Now");
+  var_dump($result);
