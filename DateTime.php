ef65df6fd242fbcd205f08758a01415556139890
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-20 13:27:11 +0700

    update DateTime.php parse date time

--- DateTime.php
+++ DateTime.php
@@ -16,3 +16,13 @@
 
   $string = $now->format("Y-m-d H:i:s");
   echo "Waktu Saat Ini : $string" . PHP_EOL;
+
+  $date = DateTime::createFromFormat("Y-m-d H:i:s", "2020-10-10 10:10:10", new DateTimeZone("Asia/Jakarta"));
+
+  if($date)
+  {
+    var_dump($date);
+  }else
+  {
+    echo "Format Salah" . PHP_EOL;
+  }
