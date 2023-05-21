e5366b12c94f26285d1a18e6f465a7566e16e7be
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-21 07:53:39 +0700

    update Reflection.php validation menggunakan reflection

--- Reflection.php
+++ Reflection.php
@@ -1,10 +1,24 @@
 <?php
-  require_once "exception/ValidationException.php";
   require_once "data/LoginRequest.php";
+  require_once "exception/ValidationException.php";
   require_once "helper/ValidationUtil.php";
 
   $request = new LoginRequest();
   $request->username = "eko";
   $request->password = "eko";
-  
-  ValidationUtil::validate($request);
+
+  ValidationUtil::validateReflection($request);
+
+  class RegisterUserRequest
+  {
+    public ?string $name;
+    public ?string $address;
+    public ?string $email;
+  }
+
+  $register = new RegisterUserRequest();
+  $register->name = "Eko";
+  $register->address = "Subang";
+  $register->email = "eko@gmail.com";
+
+  ValidationUtil::validateReflection($register);
