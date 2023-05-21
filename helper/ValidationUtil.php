6708615dc05ffa465732bc170c5a85d07c3a3d93
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-05-21 07:08:57 +0700

    add ValidationUtil.php

---
+++ helper/ValidationUtil.php
@@ -0,0 +1,12 @@
+<?php
+  class ValidationUtil
+  {
+    static function validate(LoginRequest $request)
+    {
+      if(!isset($request->username)){
+        throw new ValidationException("username is null");
+      }else if(!isset($request->password)){
+        throw new ValidationException("password is null");
+      }
+    }
+  }
